af4be91a7f8a39616de4ccd9ec88dbbb0d9e74bf
Pavel Khvalygin <hvalyginpavel@gmail.com>
2023-04-17 01:17:39 +0300

    Fix

--- internal/chain/api_out/dto/block.go
+++ internal/chain/api_out/dto/block.go
@@ -3,6 +3,7 @@ package dto
 import (
 	"bvpn-prototype/internal/protocol/entity"
 	"bvpn-prototype/internal/protocol/entity/block_data"
+	"time"
 )
 
 type BlockDto struct {
@@ -13,6 +14,54 @@ type BlockDto struct {
 	TimeStamp    int64                    `json:"timeStamp"`
 	Next         string                   `json:"next"`
 	CreatedBy    string                   `json:"createdByy"`
+}
+
+func (d *BlockDto) ToEntity() entity.Block {
+	var data []block_data.ChainStored
+	for _, dto := range d.Data {
+		switch dto.Type {
+		case block_data.TypeTransaction:
+			data = append(data, block_data.ChainStored{
+				ID:   dto.ID,
+				Type: dto.Type,
+				Data: dto.Data.(block_data.Transaction),
+			})
+			break
+		case block_data.TypeOffer:
+			data = append(data, block_data.ChainStored{
+				ID:   dto.ID,
+				Type: dto.Type,
+				Data: dto.Data.(block_data.Offer),
+			})
+			break
+		case block_data.TypeConnectionBreak:
+			data = append(data, block_data.ChainStored{
+				ID:   dto.ID,
+				Type: dto.Type,
+				Data: dto.Data.(block_data.ConnectionBreak),
+			})
+			break
+		case block_data.TypeTraffic:
+			data = append(data, block_data.ChainStored{
+				ID:   dto.ID,
+				Type: dto.Type,
+				Data: dto.Data.(block_data.Traffic),
+			})
+			break
+		default:
+			continue
+		}
+	}
+
+	return entity.Block{
+		Number:       d.Number,
+		Hash:         d.Hash,
+		PreviousHash: d.PreviousHash,
+		Data:         data,
+		TimeStamp:    time.Unix(d.TimeStamp, 0),
+		Next:         d.Next,
+		CreatedBy:    d.CreatedBy,
+	}
 }
 
 func BlockToDto(block entity.Block) BlockDto {
