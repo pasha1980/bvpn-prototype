4222ed555d6c37f9abd486d8d5a4522bb39c123d
Pavel Khvalygin <hvalyginpavel@gmail.com>
2023-04-17 01:18:47 +0300

    Add new errors in peer http api

--- internal/peer/api_in/controller.go
+++ internal/peer/api_in/controller.go
@@ -1,8 +1,9 @@
 package api_in
 
 import (
+	"bvpn-prototype/internal/infrastructure/common"
 	"bvpn-prototype/internal/infrastructure/di"
-	"bvpn-prototype/internal/infrastructure/http/http_errors"
+	"bvpn-prototype/internal/infrastructure/errors/http_errors"
 	"bvpn-prototype/internal/peer/api_in/dto"
 	"bvpn-prototype/internal/peer/domain"
 	"github.com/gofiber/fiber/v2"
@@ -13,16 +14,14 @@ type PeerController struct {
 }
 
 func (c PeerController) HealthCheck(ctx *fiber.Ctx) error {
-	return ctx.Status(http.StatusOK).JSON(map[string]bool{
-		"success": true,
-	})
+	return ctx.Status(http.StatusOK).JSON(common.MakeHttpPage(true))
 }
 
 func (c PeerController) AddPeer(ctx *fiber.Ctx) error {
 	var body dto.PeerDto
 	err := ctx.BodyParser(&body)
 	if err != nil {
-		return http_errors.ErrorInvalidRequest
+		return http_errors.InvalidRequest(err.Error())
 	}
 
 	peerService := di.Get("peer_service").(domain.PeerService)
@@ -31,9 +30,7 @@ func (c PeerController) AddPeer(ctx *fiber.Ctx) error {
 		return err
 	}
 
-	return ctx.Status(http.StatusOK).JSON(map[string]bool{
-		"success": true,
-	})
+	return ctx.Status(http.StatusOK).JSON(common.MakeHttpPage(true))
 }
 
 func NewPeerController() PeerController {
